82763e8558c2c243b61902a3f1985c56af84d7b3
Bwise1 <oguntoyebenjamin2@gmail.com>
2019-05-03 09:50:15 +0300

    Created login gui form

---
+++ src/main/java/LoginScreen.java
@@ -0,0 +1,9 @@
+import javax.swing.*;
+
+public class LoginScreen {
+    private JButton startServerButton;
+    private JTextField usernameTextField;
+    private JTextField IPAddressTextField;
+    private JButton connectButton;
+    private JTextField portTextField;
+}
